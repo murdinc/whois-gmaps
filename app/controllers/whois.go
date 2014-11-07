9c438be7acb77d9a9c89a3f33f8c888c387cfd26
Ahmad Abdo <aabdo@salon.com>
2014-11-07 23:26:58 +0000

    more progress

--- app/controllers/whois.go
+++ app/controllers/whois.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"net"
+	"github.com/robfig/revel"
+	"github.com/oschwald/geoip2-golang"
+)
 
 type Whois struct {
 	*revel.Controller
@@ -8,26 +12,33 @@ type Whois struct {
 
 func (c Whois) Index() revel.Result {
 
-	switch c.Params.Get("METHOD") {
-	case "POST":
-		revel.INFO.Print("POST")
-	case "GET":
-		revel.INFO.Print("GET")
-
+	// If this is a POST we want to return JSON info
+	if c.Params.Get("METHOD") == "POST" {
+		ip :=  net.ParseIP(c.Params.Get("ip"))
+		revel.INFO.Printf("Looking up IP address: %s %v", ip, c.Params)
+		return c.RenderJson( c.getInfo(ip) )
 	}
 
-	ip := c.Params.Get("ip")
-
-	revel.INFO.Print(ip)
-
+	// If this is a GET we want to set up our maps key
 	// Pull in our google maps api URL with key from app.conf
 	mapsApiKey, _ := revel.Config.String("app.mapsapikey")
-
 	return c.Render(mapsApiKey)
 
 }
 
-func checkIP(ip string) bool {
+func (c Whois) getInfo(ip net.IP) *geoip2.City {
+	db, err := geoip2.Open("geoip/GeoLite2-City.mmdb")
+	if err != nil {
+		revel.ERROR.Print(err)
+	}
 
-	return true
+	defer db.Close()
+	record, err := db.City(ip)
+	if err != nil {
+		revel.ERROR.Print(err)
+	}
+
+	//revel.INFO.Printf("Record: %v", record)
+
+	return record
 }
--- public/js/maps.js
+++ public/js/maps.js
@@ -1,9 +1,105 @@
 function initialize() {
-	var mapOptions = {
-		center: { lat: -34.397, lng: 150.644},
-		zoom: 8
-	};
-	var map = new google.maps.Map(document.getElementById('map-canvas'),
-			mapOptions);
+
+	var myLatLong;
+	var map;
+
+	if (navigator.geolocation) {
+		var timeoutVal = 10 * 1000 * 1000;
+		navigator.geolocation.getCurrentPosition(
+				geoSuccess, 
+				null,
+				{ enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+				);
+
+	}
+
+	function geoSuccess(position) {
+		setLatLong( position.coords.latitude, position.coords.longitude );
+		drawMap();
+
+	}
+
+	function dropPin() {
+		// To add the marker to the map, use the 'map' property
+		var marker = new google.maps.Marker({
+			position: myLatLong,
+			map: map,
+			title:"Hello World!"
+		});
+
+		var contentString = '<div id="content">'+
+			'<div id="siteNotice">'+
+			'</div>'+
+			'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+			'<div id="bodyContent">'+
+			'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+			'Heritage Site.</p>'+
+			'<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+			'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+			'(last visited June 22, 2009).</p>'+
+			'</div>'+
+			'</div>';
+
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString,
+			maxWidth: 200
+		});
+
+		infowindow.open(map,marker);
+
+		map.setCenter(myLatLong);
+
+
+	}
+
+	function setLatLong (latitude, longitude) {
+		myLatLong = new google.maps.LatLng( latitude, longitude );
+	}
+
+	function drawMap() {
+
+		var mapOptions = {
+			center: myLatLong,
+			zoom: 8
+		};
+		map = new google.maps.Map(document.getElementById('map-canvas'),
+				mapOptions);
+
+		dropPin();
+
+	}
+
+	// Attach a submit handler to the form
+	$( "#whoisForm" ).submit(function( event ) {
+
+		// Stop form from submitting normally
+		event.preventDefault();
+
+		// Get some values from elements on the page:
+		var $form = $( this ),
+	term = $form.find( "input[name='ip']" ).val(),
+	url = $form.attr( "action" );
+
+	// Send the data using post as json
+	var posting = $.post( url, { ip: term }, null, 'json' );
+
+	// Put the results in a div
+	posting.done(function( data ) {
+		//console.log( data.Location.Latitude );
+
+		// To add the marker to the map, use the 'map' property
+		var myLatLong = new google.maps.LatLng(data.Location.Latitude, data.Location.Longitude);
+
+		var marker = new google.maps.Marker({
+			position: myLatLong,
+			map: map,
+			title:"Hello World!"
+		});
+
+	});
+	});
+
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
